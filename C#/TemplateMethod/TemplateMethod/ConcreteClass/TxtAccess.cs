c209dfb4c763ca2915ed2bf5120674b04964c314
Murillo Grubler <msgrubler@gmail.com>
2017-09-09 19:33:52 -0300

    Added Design Pattern - Template Method

---
+++ C#/TemplateMethod/TemplateMethod/ConcreteClass/TxtAccess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateMethod
+{
+    public class TxtAccess : FileAccess
+    {
+        public override void Close()
+        {
+            Console.WriteLine("Close file Txt");
+        }
+
+        public override void Open()
+        {
+            Console.WriteLine("Open file Txt");
+        }
+
+        public override string Read()
+        {
+            return "Txt info...";
+        }
+    }
+}
---
+++ C#/TemplateMethod/TemplateMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileAccess txt = new TxtAccess();
+            Console.WriteLine(txt.Execute());
+
+            FileAccess csv = new CsvAccess();
+            Console.WriteLine(csv.Execute());
+
+            Console.ReadKey();
+        }
+    }
+}
