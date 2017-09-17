f41e3652b9b6f46c0d307c745bef7ac466bb4d71
Murillo Grubler <msgrubler@gmail.com>
2017-09-17 17:30:31 -0300

    Added design pattern - Facade

---
+++ C#/Structural Patterns/Facade/Facade/Subsystem/Customer.cs
@@ -0,0 +1,19 @@
+﻿namespace Facade
+{
+    /// <summary>
+    /// This class represent an union of properties that return 
+    /// informations about the customer.
+    /// </summary>
+    public class Customer
+    {
+        public string Name { get; set; }
+
+        public int Code { get; set; }
+
+        public Customer(int code)
+        {
+            Code = code;
+            Name = $"Customer {code}";
+        }
+    }
+}
